f8d5df9dea27a7b1b287f6e27ce13c977c48af9a
Shrijan Paudel <102872515+SrijanPoudel@users.noreply.github.com>
2025-02-01 14:46:30 -0600

    Defense of console

---
+++ DefenseConsole.cs
@@ -0,0 +1,29 @@
+﻿//Written by Shrijan Paudel
+//01/27/2025
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Defense_of_Consolas
+{
+        public class DefenseDeployment
+        {
+            // Method to deploy defense based on target row and column
+            public void DeployDefense(int targetRow, int targetColumn)
+            {
+                Console.ForegroundColor = ConsoleColor.Green; // Change console color to Green
+
+                Console.WriteLine("Deploying Defense..");
+                Console.WriteLine($"({targetRow},{targetColumn - 1})"); // Left row
+                Console.WriteLine($"({targetRow - 1},{targetColumn})"); // Above target
+                Console.WriteLine($"({targetRow},{targetColumn + 1})"); // Right rpw
+                Console.WriteLine($"({targetRow + 1},{targetColumn})"); // Below target
+
+                Console.Beep(); // Beep sound for deployment notification
+            }
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//Written by Shrijan Paudel
+//DAte:1/27
+
+
+//Defense  of Consolas Program 
+
+using System;
+using The_Defense_of_Consolas;
+
+namespace DefenseOfConsolas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asking for the target row and column
+            Console.Write("Target Row? ");
+            int targetRow = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Target Column? ");
+            int targetColumn = Convert.ToInt32(Console.ReadLine());
+
+            Console.Title = "DefenseOfConsolas"; // title of the console
+
+
+            DefenseDeployment deployment = new DefenseDeployment();
+
+            deployment.DeployDefense(targetRow, targetColumn); // calling method with parameters
+
+
+        }
+    }
+}
